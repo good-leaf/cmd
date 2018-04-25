61e1ee33cc758b93eb86dcafba6273c3acfdc568
yangyajun03 <yangyajun03@meituan.com>
2018-04-25 14:42:11 +0800

    init

---
+++ Makefile
@@ -0,0 +1,8 @@
+PROJECT = cmd
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.1.0
+
+DEPS = eper
+LOCAL_DEPS = tools
+
+include erlang.mk
---
+++ src/cmd_app.erl
@@ -0,0 +1,11 @@
+-module(cmd_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	cmd_sup:start_link().
+
+stop(_State) ->
+	ok.
